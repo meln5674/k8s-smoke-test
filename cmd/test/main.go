b7f9e2de0af0c4b3ffa4fea3e8e56751f4dd5413
Andrew Melnick <meln5674@kettering.edu>
2023-12-19 18:46:45 -0700

    Add test with port-forwarding

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -9,22 +9,26 @@ import (
 
 	flag "github.com/spf13/pflag"
 
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 
 	"github.com/meln5674/k8s-smoke-test/pkg/test"
 )
 
 var (
-	releaseName         = flag.String("release-name", "k8s-smoke-test", "Name of the release")
-	mergedValuesPath    = flag.String("merged-values-json", "-", "Path to the merged helm values, in JSON format, or `-` for STDIN")
-	kubeconfig          = flag.String("kubeconfig", os.Getenv("KUBECONFIG"), "Path to the kubeconfig file to use for CLI requests.")
-	kubernetesOverrides clientcmd.ConfigOverrides
+	releaseName          = flag.String("release-name", "k8s-smoke-test", "Name of the release")
+	mergedValuesPath     = flag.String("merged-values-json", "-", "Path to the merged helm values, in JSON format, or `-` for STDIN")
+	kubeconfig           = flag.String("kubeconfig", os.Getenv("KUBECONFIG"), "Path to the kubeconfig file to use for CLI requests.")
+	portForwardLocalPort = flag.Int("port-forward-local-port", 8080, "Local port to use when testing port-forwarding")
+	kubernetesOverrides  clientcmd.ConfigOverrides
 )
+
+func init() {
+	clientcmd.BindOverrideFlags(&kubernetesOverrides, flag.CommandLine, clientcmd.RecommendedConfigOverrideFlags(""))
+	flag.Parse()
+}
 
 func main() {
 	ctx := context.Background()
-	clientcmd.BindOverrideFlags(&kubernetesOverrides, flag.CommandLine, clientcmd.RecommendedConfigOverrideFlags(""))
 	var err error
 	mergedValuesStream := os.Stdin
 	if *mergedValuesPath != "-" {
@@ -57,12 +61,14 @@ func main() {
 		log.Fatal(err)
 	}
 
-	k8sClient, err := kubernetes.NewForConfig(clientConfig)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = test.Test(ctx, http.DefaultClient, k8sClient, namespace, *releaseName, &mergedValues)
+	err = test.Test(ctx, &test.Config{
+		HTTP:                 http.DefaultClient,
+		K8sConfig:            clientConfig,
+		ReleaseNamespace:     namespace,
+		ReleaseName:          *releaseName,
+		MergedValues:         &mergedValues,
+		PortForwardLocalPort: *portForwardLocalPort,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
--- pkg/test/test.go
+++ pkg/test/test.go
@@ -5,11 +5,21 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
 	"strings"
 
+	"github.com/pkg/errors"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/portforward"
+	"k8s.io/client-go/transport/spdy"
 )
 
 func testURL(errName string, url string, resp *http.Response, err error, expectedRespBody string) error {
@@ -60,42 +70,121 @@ type StatefulSetValues struct {
 	NodePortHostname string `json:"nodePortHostname"`
 }
 
-func Test(ctx context.Context, httpClient *http.Client, k8sClient *kubernetes.Clientset, releaseNamespace string, releaseName string, mergedValues *MergedValues) error {
-	fullname := mergedValues.FullnameOverride
+func portForward(ctx context.Context, k8sConfig *rest.Config, namespace, pod string, ports []string, f func() error) error {
+	portForwardURL, err := url.Parse(k8sConfig.Host)
+	if err != nil {
+		return errors.Wrap(err, "Failed to parse Kubernetes server URL")
+	}
+	portForwardURL.Path = filepath.Join(portForwardURL.Path, k8sConfig.APIPath, "/api/v1/namespaces/", namespace, "/pods/", pod, "/portforward")
+
+	roundTripper, upgrader, err := spdy.RoundTripperFor(k8sConfig)
+	if err != nil {
+		return errors.Wrapf(err, "Failed to create SPDY round-tripper for port-forward at URL %s", portForwardURL)
+	}
+	dialer := spdy.NewDialer(upgrader, &http.Client{Transport: roundTripper}, http.MethodPost, portForwardURL)
+
+	log.Println("Beginning port-forward...")
+
+	ready := make(chan struct{})
+	stop := make(chan struct{})
+	defer close(stop)
+	errChan := make(chan error)
+	forwarder, err := portforward.New(dialer, ports, stop, ready, os.Stdout, os.Stderr)
+	if err != nil {
+		return err
+	}
+	defer forwarder.Close()
+	go func() {
+		errChan <- forwarder.ForwardPorts()
+	}()
+	select {
+	case err = <-errChan:
+		return err
+	case <-ctx.Done():
+		return context.Canceled
+	case <-ready:
+	}
+	return f()
+}
+
+type Config struct {
+	HTTP                 *http.Client
+	K8sConfig            *rest.Config
+	ReleaseNamespace     string
+	ReleaseName          string
+	MergedValues         *MergedValues
+	PortForwardLocalPort int
+}
+
+func Test(ctx context.Context, cfg *Config) error {
+	k8sClient, err := kubernetes.NewForConfig(cfg.K8sConfig)
+	if err != nil {
+		return errors.Wrap(err, "Failed to create Kubernetes client")
+	}
+
+	fullname := cfg.MergedValues.FullnameOverride
 	if fullname == "" {
-		if strings.Contains(releaseName, "k8s-smoke-test") {
-			fullname = releaseName
+		if strings.Contains(cfg.ReleaseName, "k8s-smoke-test") {
+			fullname = cfg.ReleaseName
 		} else {
-			fullname = releaseName + "-k8s-smoke-test"
+			fullname = cfg.ReleaseName + "-k8s-smoke-test"
 		}
 	}
 
-	ingressHostname := mergedValues.Deployment.Ingress.Hostname
+	log.Println("Finding pod to port-forward...")
+	deploymentService, err := k8sClient.CoreV1().Services(cfg.ReleaseNamespace).Get(ctx, fmt.Sprintf("%s-deployment", fullname), metav1.GetOptions{})
+	if err != nil {
+		return errors.Wrap(err, "Failed to get deployment service")
+	}
+	deploymentPods, err := k8sClient.CoreV1().Pods(cfg.ReleaseNamespace).List(ctx, metav1.ListOptions{
+		LabelSelector: labels.FormatLabels(deploymentService.Spec.Selector),
+	})
+	if err != nil {
+		return errors.Wrap(err, "Failed to list deployment pods")
+	}
+	if len(deploymentPods.Items) == 0 {
+		return errors.New("No deployment pods were present")
+	}
+	pod := deploymentPods.Items[0]
+
+	log.Printf("Found pod %s to port-forward...", pod.Name)
+
+	err = portForward(ctx, cfg.K8sConfig, cfg.ReleaseNamespace, pod.Name, []string{fmt.Sprintf("%d:8080", cfg.PortForwardLocalPort)}, func() error {
+		portForwardURL := fmt.Sprintf("http://localhost:%d/rwx/%s", cfg.PortForwardLocalPort, cfg.MergedValues.TestFile.Name)
+		resp, err := http.Get(portForwardURL)
+		err = testURL("GET RWO Port-Forward", portForwardURL, resp, err, cfg.MergedValues.TestFile.Contents)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	ingressHostname := cfg.MergedValues.Deployment.Ingress.Hostname
 	ingressProtocol := "http"
-	if len(mergedValues.Deployment.Ingress.TLS) != 0 {
+	if len(cfg.MergedValues.Deployment.Ingress.TLS) != 0 {
 		ingressProtocol = "https"
 	}
-	ingressURL := fmt.Sprintf("%s://%s/rwx/%s", ingressProtocol, ingressHostname, mergedValues.TestFile.Name)
+	ingressURL := fmt.Sprintf("%s://%s/rwx/%s", ingressProtocol, ingressHostname, cfg.MergedValues.TestFile.Name)
 	resp, err := http.Get(ingressURL)
-	err = testURL("GET RWO Ingress", ingressURL, resp, err, mergedValues.TestFile.Contents)
+	err = testURL("GET RWO Ingress", ingressURL, resp, err, cfg.MergedValues.TestFile.Contents)
 	if err != nil {
 		return err
 	}
 
-	loadBalancer, err := k8sClient.CoreV1().Services(releaseNamespace).Get(ctx, fullname+"-statefulset", metav1.GetOptions{})
+	loadBalancer, err := k8sClient.CoreV1().Services(cfg.ReleaseNamespace).Get(ctx, fullname+"-statefulset", metav1.GetOptions{})
 	if err != nil {
 		return fmt.Errorf("LoadBalancer service does not exist: %s", err)
 	}
 
-	nodePortHostname := mergedValues.StatefulSet.NodePortHostname
+	nodePortHostname := cfg.MergedValues.StatefulSet.NodePortHostname
 	nodePort := loadBalancer.Spec.Ports[0].NodePort
 	if nodePort == 0 {
 		return fmt.Errorf("LoadBalancer service does not have a nodePort assigned")
 	}
 
-	nodePortURL := fmt.Sprintf("http://%s:%d/rwx/%s", nodePortHostname, nodePort, mergedValues.TestFile.Name)
-	resp, err = httpClient.Get(nodePortURL)
-	err = testURL("GET RWX NodePort", nodePortURL, resp, err, mergedValues.TestFile.Contents)
+	nodePortURL := fmt.Sprintf("http://%s:%d/rwx/%s", nodePortHostname, nodePort, cfg.MergedValues.TestFile.Name)
+	resp, err = cfg.HTTP.Get(nodePortURL)
+	err = testURL("GET RWX NodePort", nodePortURL, resp, err, cfg.MergedValues.TestFile.Contents)
 	if err != nil {
 		return err
 	}
@@ -125,23 +214,23 @@ func Test(ctx context.Context, httpClient *http.Client, k8sClient *kubernetes.Cl
 			return fmt.Errorf("LoadBalancer servce ingress at index %d has no port assigned", ix)
 		}
 
-		loadBalancerURL := fmt.Sprintf("http://%s:%d/rwx/%s", hostname, port, mergedValues.TestFile.Name)
-		resp, err = httpClient.Get(loadBalancerURL)
-		err = testURL(fmt.Sprintf("GET RWX LoadBalancer ingress index %d", ix), loadBalancerURL, resp, err, mergedValues.TestFile.Contents)
+		loadBalancerURL := fmt.Sprintf("http://%s:%d/rwx/%s", hostname, port, cfg.MergedValues.TestFile.Name)
+		resp, err = cfg.HTTP.Get(loadBalancerURL)
+		err = testURL(fmt.Sprintf("GET RWX LoadBalancer ingress index %d", ix), loadBalancerURL, resp, err, cfg.MergedValues.TestFile.Contents)
 		if err != nil {
 			return err
 		}
 
-		loadBalancerURL = fmt.Sprintf("http://%s:%d/rwo/%s", hostname, port, mergedValues.TestFile.Name)
-		resp, err = httpClient.Post(loadBalancerURL, "application/octet-stream", bytes.NewBuffer([]byte(mergedValues.TestFile.Contents)))
+		loadBalancerURL = fmt.Sprintf("http://%s:%d/rwo/%s", hostname, port, cfg.MergedValues.TestFile.Name)
+		resp, err = cfg.HTTP.Post(loadBalancerURL, "application/octet-stream", bytes.NewBuffer([]byte(cfg.MergedValues.TestFile.Contents)))
 		err = testURL(fmt.Sprintf("POST RWO LoadBalancer ingress index %d", ix), loadBalancerURL, resp, err, "")
 		if err != nil {
 			return err
 		}
 
-		loadBalancerURL = fmt.Sprintf("http://%s:%d/rwo/%s", hostname, port, mergedValues.TestFile.Name)
-		resp, err = httpClient.Get(loadBalancerURL)
-		err = testURL(fmt.Sprintf("GET RWO LoadBalancer ingress index %d", ix), loadBalancerURL, resp, err, mergedValues.TestFile.Contents)
+		loadBalancerURL = fmt.Sprintf("http://%s:%d/rwo/%s", hostname, port, cfg.MergedValues.TestFile.Name)
+		resp, err = cfg.HTTP.Get(loadBalancerURL)
+		err = testURL(fmt.Sprintf("GET RWO LoadBalancer ingress index %d", ix), loadBalancerURL, resp, err, cfg.MergedValues.TestFile.Contents)
 		if err != nil {
 			return err
 		}
